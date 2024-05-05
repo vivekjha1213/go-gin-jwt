d6f94331d1caaf5f5ccde4cbb3d45d1cfdeef928
vivek jha <vivekjha1213@gmail.com>
2024-05-05 14:31:01 +0530

    Added updated service

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,14 +1,11 @@
 package helper
 
-import(
+import (
 	"errors"
 	"github.com/gin-gonic/gin"
 )
 
-
-
-
-func CheckUserType(c *gin.Context, role string) (err error){
+func CheckUserType(c *gin.Context, role string) (err error) {
 	userType := c.GetString("user_type")
 	err = nil
 	if userType != role {
@@ -18,10 +15,10 @@ func CheckUserType(c *gin.Context, role string) (err error){
 	return err
 }
 
-func MatchUserTypeToUid(c *gin.Context, userId string) (err error){
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
 	userType := c.GetString("user_type")
 	uid := c.GetString("uid")
-	err= nil
+	err = nil
 
 	if userType == "USER" && uid != userId {
 		err = errors.New("Unauthorized to access this resource")
